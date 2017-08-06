c11668335a705eda6c9434c1553c74199b54fe4f
cash <cash@cashwu.com>
2017-08-06 18:21:03 +0800

    input 1 1 2 test case

--- Kata20170806_/IQTests.cs
+++ Kata20170806_/IQTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170806_
     public class IQTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_1_2_should_return_2()
         {
+            IQTestSShouldBe(2, "1 1 2");
+        }
+
+        private static void IQTestSShouldBe(int expected, string numbers)
+        {
+            var iq = new IQ();
+            var actual = iq.Test(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class IQ
+    {
+        public int Test(string numbers)
+        {
+            return 2;
         }
     }
 }
