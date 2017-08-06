a363a3acc89b0a8a952d4bd06659e33592cefbf4
cash <cash@cashwu.com>
2017-08-06 18:14:24 +0800

    init

--- Kata20170806_/IQTests.cs
+++ Kata20170806_/IQTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170806_
 {
     [TestClass]
-    public class UnitTest1
+    public class IQTests
     {
         [TestMethod]
         public void TestMethod1()
